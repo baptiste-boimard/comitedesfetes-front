a9191bc39b4a30e80a452f6f0fe55417c6e0860b
baptiste-boimard <bouketin27@gmail.com>
2023-07-06 10:27:04 +0200

    Ajout de l'identification par aws

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+  cognito: {
+      userPoolId: 'eu-west-3_kwlzo7hes', // e.g. us-east-2_uXboG5pAb
+      userPoolClientId: '2bh2p5g4chl246ptqt8n4vtskn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+      region: 'eu-west-3' // e.g. us-east-2
+  },
+  api: {
+      invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  }
+};
